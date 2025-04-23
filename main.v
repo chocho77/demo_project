c9d36449712b5ce99589cbc0f48f18e791546ac1
Chavdar Momchilov <chavmom@gmail.com>
2025-04-23 21:31:11 +0300

    new file:   .editorconfig 	new file:   .gitattributes 	new file:   .gitignore 	new file:   main.v 	new file:   v.mod

---
+++ main.v
@@ -0,0 +1,13 @@
+module main
+
+fn main() {
+	println('Hello World!')
+    println('Variables')
+	name := 'Bob'
+    age := 20
+    large_number := i64(9999999999)
+    println(name) 
+    println(age)
+    println(large_number)
+
+}
---
+++ v.mod
@@ -0,0 +1,7 @@
+Module {
+	name: 'demo_project'
+	description: 'demo'
+	version: '0.0.0'
+	license: 'MIT'
+	dependencies: []
+}
